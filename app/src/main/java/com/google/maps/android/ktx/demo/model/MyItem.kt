be9b1a6b2089e9197f78efc5ada875a8f385247d
Sean Barbeau <sjbarbeau@gmail.com>
2020-04-01 11:41:46 -0400

    Add comment on Kotlin data classes and Java interface methods

--- app/src/main/java/com/google/maps/android/ktx/demo/model/MyItem.kt
+++ app/src/main/java/com/google/maps/android/ktx/demo/model/MyItem.kt
@@ -4,6 +4,9 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterItem
 
 data class MyItem (val latLng: LatLng, val myTitle: String?, val mySnippet: String?) : ClusterItem {
+    // Unfortunately Kotlin data class fields override Java interface methods by default
+    // (https://youtrack.jetbrains.com/issue/KT-6653?_ga=2.30406975.1494223917.1585591891-1137021041.1573759593)
+    // so we must map our fields with different names to the ClusterItem methods
     override fun getPosition() = latLng
     override fun getTitle() = myTitle
     override fun getSnippet() = mySnippet
