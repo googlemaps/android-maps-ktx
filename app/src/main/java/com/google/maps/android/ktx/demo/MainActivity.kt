4816493340abbb74f727e088e9f83fedab87cc55
Sean Barbeau <sjbarbeau@gmail.com>
2020-04-01 11:32:36 -0400

    Add working clusters, awaitMap()

--- app/src/main/java/com/google/maps/android/ktx/demo/MainActivity.kt
+++ app/src/main/java/com/google/maps/android/ktx/demo/MainActivity.kt
@@ -2,62 +2,61 @@ package com.google.maps.android.ktx.demo
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.lifecycle.coroutineScope
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.collections.GroundOverlayManager
 import com.google.maps.android.collections.MarkerManager
 import com.google.maps.android.collections.PolygonManager
 import com.google.maps.android.collections.PolylineManager
+import com.google.maps.android.ktx.MapsExperimentalFeature
+import com.google.maps.android.ktx.awaitMap
 import com.google.maps.android.ktx.demo.model.MyItem
 import org.json.JSONException
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
     var mIsRestore: Boolean = false
 
+    @MapsExperimentalFeature
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mIsRestore = savedInstanceState != null
         setContentView(R.layout.activity_main)
-        val mapFragment : SupportMapFragment? =
-            supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
-    }
-
-    override fun onMapReady(map: GoogleMap?) {
-        map?: return
-        if (!mIsRestore) {
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(51.403186, -0.126446),10F))
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        lifecycle.coroutineScope.launchWhenCreated {
+            check(mapFragment != null)
+            val googleMap = mapFragment.awaitMap()
+            if (!mIsRestore) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(51.403186, -0.126446),10F))
+            }
+            showMapLayers(googleMap)
         }
-        showMapLayers(map)
     }
 
     private fun showMapLayers(map: GoogleMap) {
-
         // Shared object managers - used to support multiple layer types on the map simultaneously
         val markerManager = MarkerManager(map)
         val groundOverlayManager = GroundOverlayManager(map)
         val polygonManager = PolygonManager(map)
         val polylineManager = PolylineManager(map)
 
-        // Add clustering
-        val clusterManager = ClusterManager<MyItem>(this, map, markerManager)
-        map.setOnCameraIdleListener(clusterManager)
-        //addClusterItems(clusterManager)
+        addClusters(map, markerManager)
     }
 
-//    private fun addClusterItems(clusterManager: ClusterManager<*>) {
-//        val inputStream = resources.openRawResource(R.raw.radar_search)
-//        try {
-//            val items = MyItemReader().read(inputStream)
-//            clusterManager.addItems(items)
-//        } catch (e: JSONException) {
-//            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show()
-//            e.printStackTrace()
-//        }
-//    }
+    private fun addClusters(map: GoogleMap, markerManager: MarkerManager) {
+        val clusterManager = ClusterManager<MyItem>(this, map, markerManager)
+        map.setOnCameraIdleListener(clusterManager)
+
+        try {
+            val items = MyItemReader().read(resources.openRawResource(R.raw.radar_search))
+            clusterManager.addItems(items)
+        } catch (e: JSONException) {
+            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
+    }
 }
