f1694134adc703fa445a5bdd13b63cf044b404c9
Sean Barbeau <sjbarbeau@gmail.com>
2020-04-02 10:38:45 -0400

    Fix comment

--- app/src/main/java/com/google/maps/android/ktx/demo/model/MyItem.kt
+++ app/src/main/java/com/google/maps/android/ktx/demo/model/MyItem.kt
@@ -6,9 +6,9 @@ import com.google.maps.android.clustering.ClusterItem
 /**
  * A data class that implements the [ClusterItem] interface so it can be clustered.
  *
- *  Unfortunately Kotlin data class fields override Java interface methods by default
+ *  Unfortunately Kotlin data class fields do not override Java interface methods by default
  *  (https://youtrack.jetbrains.com/issue/KT-6653?_ga=2.30406975.1494223917.1585591891-1137021041.1573759593)
- *  so we must map our fields with different names to the ClusterItem methods
+ *  so we must name our fields differently and then pass them to the ClusterItem methods.
  */
 data class MyItem(val latLng: LatLng, val myTitle: String?, val mySnippet: String?) : ClusterItem {
     override fun getPosition() = latLng
