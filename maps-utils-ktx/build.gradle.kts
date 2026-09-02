a8c60a47493923690254c2c7b13603c2da2635a9
Dale Hawkins <107309+dkhawk@users.noreply.github.com>
2026-09-02 11:23:23 -0600

    fix: retain JVM bytecode target at Java 11
    Release-As: 6.4.1

--- maps-utils-ktx/build.gradle.kts
+++ maps-utils-ktx/build.gradle.kts
@@ -48,14 +48,14 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlin {
         compilerOptions {
             freeCompilerArgs.add("-Xexplicit-api=strict")
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
 
